492b3f71a592368c8a1773338c62a68301aa878e
willianmarcolin <willianmarcolin@hotmail.com>
2017-10-05 22:01:23 -0300

    Adicionado teste kkk

--- app/src/main/java/br/com/uniftec/projetoecommerce/MainActivity.java
+++ app/src/main/java/br/com/uniftec/projetoecommerce/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     // Hello word!
+    // Hello Will!
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
